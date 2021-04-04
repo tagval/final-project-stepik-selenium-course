b38d768763b4a6b6addca9f51fad936eaa817ca1
tagval <elanskaiavaleriia@gmail.com>
2021-04-04 19:24:45 +0300

    last step when it works together

--- lesson_4_1/conftest.py
+++ lesson_4_1/conftest.py
@@ -1,23 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en-gb",
-                     help="Choose language: ru, en-gb, ar, etc.")
+                     help="Choose language: ru, en, ar, etc.")
 
 @pytest.fixture(scope="function")
-def language(request):
-    l=["ar", "ca", "cs", "da", "de", "en", "el", "es", "fi", "fr", "it", "ko", "nl", "pl", "pt", "pt-br", "ro", "ru", "sk", "uk", "zh-hans"]
+def browser(request):
     language = request.config.getoption("language")
-    if language in l:
-        return language
-    else:
-        raise pytest.UsageError("--language should be correct")
-
-@pytest.fixture(scope="function")
-def browser():
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome('C:/chromedriver/chromedriver', options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- lesson_4_1/test_main_page.py
+++ lesson_4_1/test_main_page.py
@@ -1,6 +1,8 @@
+import time
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    time.sleep(10)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
