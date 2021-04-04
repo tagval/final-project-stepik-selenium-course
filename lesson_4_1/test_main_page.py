9de503e5f4c17ae32a275f1f05477d4822fd63cd
tagval <elanskaiavaleriia@gmail.com>
2021-04-04 19:08:15 +0300

    some changes

--- lesson_4_1/test_main_page.py
+++ lesson_4_1/test_main_page.py
@@ -3,3 +3,5 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+    # pytest -v --tb=line --language=en lesson_4_1/test_main_page.py
